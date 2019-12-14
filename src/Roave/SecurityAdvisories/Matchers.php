e618f39d4ae685c2b9b676f2454c277dd17c3b5a
Ilya Tribusean <slash3b@gmail.com>
2019-12-14 13:01:14 +0200

    issue/91. todo comment

--- src/Roave/SecurityAdvisories/Matchers.php
+++ src/Roave/SecurityAdvisories/Matchers.php
@@ -20,6 +20,11 @@ declare(strict_types=1);
 
 namespace Roave\SecurityAdvisories;
 
+/**
+ * @fixme: throw this garbage away and use existing regexp from semver.org
+ * @fixme: we could declare it directly in build-conflicts.php
+ * @see https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
+ */
 final class Matchers
 {
     // pattern that matches full version only, without boundary sign

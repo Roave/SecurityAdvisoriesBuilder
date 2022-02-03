4878c8815f6a90029442d9f784eae955725aaaa1
Benjamin Fahl <git@fahl-design.de>
2022-02-03 22:30:55 +0100

    Add "GetAdvisoriesAdvisoryRuleDecorator" to apply "AdvisoryRules"
    - add unit tests to verify rules are applied as expected
    
    ref Roave/SecurityAdvisories#90
    
    Signed-off-by: Benjamin Fahl <git@fahl-design.de>

---
+++ test/RoaveTest/SecurityAdvisories/AdvisorySources/GetAdvisoriesOverwriteVersionConstraintsDecoratorTest.php
@@ -0,0 +1,361 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
+ */
+
+declare(strict_types=1);
+
+namespace RoaveTest\SecurityAdvisories\AdvisorySources;
+
+use Generator;
+use PHPUnit\Framework\TestCase;
+use Psl\Vec;
+use Roave\SecurityAdvisories\Advisory;
+use Roave\SecurityAdvisories\AdvisoryRule;
+use Roave\SecurityAdvisories\AdvisorySources\GetAdvisories;
+use Roave\SecurityAdvisories\AdvisorySources\GetAdvisoriesAdvisoryRuleDecorator;
+use Roave\SecurityAdvisories\PackageName;
+use Roave\SecurityAdvisories\Rule\AdvisoryRuleProvider;
+
+use function assert;
+use function method_exists;
+
+class GetAdvisoriesOverwriteVersionConstraintsDecoratorTest extends TestCase
+{
+    public function testThatAdvisoriesAreDecoratedAfterBuiltFromYamlFilesAndConstraintIsChanged(): void
+    {
+        // Arrange
+        $advisories = $this->getTempProvideGetAdvisories();
+
+        $ruleToChangeLowerVersionConstraintRule = new AdvisoryRule(
+            PackageName::fromName('3f/pygmentize'),
+            '<1.2',
+            [
+                '1.x' => [
+                    'versions' => ['<1.1'],
+                ],
+                '2.x' => [
+                    'versions' => ['>2.0'],
+                ],
+            ],
+        );
+
+        $overwriteRuleProvider = new AdvisoryRuleProvider([$ruleToChangeLowerVersionConstraintRule]);
+
+        $decoratedAdvisories = (new GetAdvisoriesAdvisoryRuleDecorator(
+            $advisories,
+            $overwriteRuleProvider,
+        ));
+
+        // Act
+        $notDecoratedAdvisories = $advisories();
+        $decoratedAdvisories    = $decoratedAdvisories();
+
+        // Assert
+        // check default handling
+        self::assertEquals([
+            Advisory::fromArrayData([
+                'branches' => [
+                    '1.x' => [
+                        'time' => '2017-05-15 09:09:00',
+                        'versions' => ['<1.2'],
+                    ],
+                ],
+                'reference' => 'composer://3f/pygmentize',
+            ]),
+            Advisory::fromArrayData([
+                'branches' => [
+                    '3.x' => [
+                        'time' => '2012-05-15 09:09:00',
+                        'versions' => ['<2|>4'],
+                    ],
+                ],
+                'reference' => 'composer://other/package-name',
+            ]),
+        ], Vec\values($notDecoratedAdvisories));
+
+        // check decorated handling and see version lowered and one branch added
+        self::assertEquals([
+            Advisory::fromArrayData([
+                'branches' => [
+                    '1.x' => [
+                        'time' => '2017-05-15 09:09:00',
+                        'versions' => ['<1.1'],
+                    ],
+                    '2.x' => [
+                        'time' => '2017-05-15 09:09:00',
+                        'versions' => ['>2.0'],
+                    ],
+                ],
+                'reference' => 'composer://3f/pygmentize',
+            ]),
+            Advisory::fromArrayData([
+                'branches' => [
+                    '3.x' => [
+                        'time' => '2012-05-15 09:09:00',
+                        'versions' => ['<2|>4'],
+                    ],
+                ],
+                'reference' => 'composer://other/package-name',
+            ]),
+        ], Vec\values($decoratedAdvisories));
+    }
+
+    public function testThatNothingBreaksWhenNoRulesAreProvided(): void
+    {
+        // Arrange
+        $advisories = $this->getTempProvideGetAdvisories();
+
+        $overwriteRuleProvider = new AdvisoryRuleProvider([]);
+
+        $decoratedAdvisories = (new GetAdvisoriesAdvisoryRuleDecorator(
+            $advisories,
+            $overwriteRuleProvider,
+        ));
+
+        // Act
+        $notDecoratedAdvisories = $advisories();
+        $decoratedAdvisories    = $decoratedAdvisories();
+
+        // Assert
+        // check default handling
+        self::assertEquals([
+            Advisory::fromArrayData([
+                'branches' => [
+                    '1.x' => [
+                        'time' => '2017-05-15 09:09:00',
+                        'versions' => ['<1.2'],
+                    ],
+                ],
+                'reference' => 'composer://3f/pygmentize',
+            ]),
+            Advisory::fromArrayData([
+                'branches' => [
+                    '3.x' => [
+                        'time' => '2012-05-15 09:09:00',
+                        'versions' => ['<2|>4'],
+                    ],
+                ],
+                'reference' => 'composer://other/package-name',
+            ]),
+        ], Vec\values($notDecoratedAdvisories));
+
+        // check decorated handling and see version lowered and one branch added
+        self::assertEquals([
+            Advisory::fromArrayData([
+                'branches' => [
+                    '1.x' => [
+                        'time' => '2017-05-15 09:09:00',
+                        'versions' => ['<1.2'],
+                    ],
+                ],
+                'reference' => 'composer://3f/pygmentize',
+            ]),
+            Advisory::fromArrayData([
+                'branches' => [
+                    '3.x' => [
+                        'time' => '2012-05-15 09:09:00',
+                        'versions' => ['<2|>4'],
+                    ],
+                ],
+                'reference' => 'composer://other/package-name',
+            ]),
+        ], Vec\values($decoratedAdvisories));
+    }
+
+    public function testThatRuleIsAddedToExpectedAdvisory(): void
+    {
+        // Arrange
+        $ruleToChangeLowerVersionConstraintRule = new AdvisoryRule(
+            PackageName::fromName('3f/pygmentize'),
+            '<1.2',
+            [
+                '1.x' => [
+                    'versions' => ['<1.0|>2.0'],
+                ],
+            ],
+        );
+
+        $overwriteRuleProvider = new AdvisoryRuleProvider([$ruleToChangeLowerVersionConstraintRule]);
+
+        $getAdvisories = $this->getTempProvideGetAdvisories();
+
+        $decoratedAdvisories = (new GetAdvisoriesAdvisoryRuleDecorator(
+            $getAdvisories,
+            $overwriteRuleProvider,
+        ));
+
+        // Act
+        $decoratedAdvisories = $decoratedAdvisories();
+
+        // Assert
+        // check decorated handling and see version lowered and one branch added
+        self::assertEquals([
+            Advisory::fromArrayData([
+                'branches' => [
+                    '1.x' => [
+                        'time' => '2017-05-15 09:09:00',
+                        'versions' => ['<1.0|>2.0'],
+                    ],
+                ],
+                'reference' => 'composer://3f/pygmentize',
+            ]),
+            Advisory::fromArrayData([
+                'branches' => [
+                    '3.x' => [
+                        'time' => '2012-05-15 09:09:00',
+                        'versions' => ['<2|>4'],
+                    ],
+                ],
+                'reference' => 'composer://other/package-name',
+            ]),
+        ], Vec\values($decoratedAdvisories));
+    }
+
+    public function testThatRuleToOverwriteLaminasFormConstrainsWorksAsExpected(): void
+    {
+        // Arrange
+        $ruleToFixLaminasFormConstraint = new AdvisoryRule(
+            PackageName::fromName('laminas/laminas-form'),
+            '<2.17.2|>=3,<3.0.2|>=3.1,<3.1.1',
+            [
+                '2.17.x' => [
+                    'versions' => ['<2.17.1'],
+                ],
+                '3.0.x' => [
+                    'versions' => ['>=3','<3.0.2'],
+                ],
+                '3.1.x' => [
+                    'versions' => ['>=3.1','<3.1.1'],
+                ],
+            ],
+        );
+
+        $overwriteRuleProvider = new AdvisoryRuleProvider([$ruleToFixLaminasFormConstraint]);
+
+        $getAdvisories = $this->getTempProvideGetAdvisories();
+
+        assert(method_exists($getAdvisories, 'setAdvisory'));
+        $getAdvisories->setAdvisory(
+            Advisory::fromArrayData([
+                'branches' => [
+                    '2.17.x' => [
+                        'versions' => ['<2.17.2'],
+                    ],
+                    '3.0.x' => [
+                        'versions' => ['>=3','<3.0.2'],
+                    ],
+                    '3.1.x' => [
+                        'versions' => ['>=3.1','<3.1.1'],
+                    ],
+                ],
+                'reference' => 'composer://laminas/laminas-form',
+            ]),
+        );
+
+        $decoratedAdvisories = (new GetAdvisoriesAdvisoryRuleDecorator(
+            $getAdvisories,
+            $overwriteRuleProvider,
+        ));
+
+        // Act
+        $decoratedAdvisories = $decoratedAdvisories();
+
+        // Assert
+        // check decorated handling and see version lowered and one branch added
+        self::assertEquals([
+            Advisory::fromArrayData([
+                'branches' => [
+                    '1.x' => [
+                        'time' => '2017-05-15 09:09:00',
+                        'versions' => ['<1.2'],
+                    ],
+                ],
+                'reference' => 'composer://3f/pygmentize',
+            ]),
+            Advisory::fromArrayData([
+                'branches' => [
+                    '3.x' => [
+                        'time' => '2012-05-15 09:09:00',
+                        'versions' => ['<2|>4'],
+                    ],
+                ],
+                'reference' => 'composer://other/package-name',
+            ]),
+            Advisory::fromArrayData([
+                'branches' => [
+                    '2.17.x' => [
+                        'versions' => ['<2.17.1'],
+                    ],
+                    '3.0.x' => [
+                        'versions' => ['>=3','<3.0.2'],
+                    ],
+                    '3.1.x' => [
+                        'versions' => ['>=3.1','<3.1.1'],
+                    ],
+                ],
+                'reference' => 'composer://laminas/laminas-form',
+            ]),
+        ], Vec\values($decoratedAdvisories));
+    }
+
+    private function getTempProvideGetAdvisories(): GetAdvisories
+    {
+        return new class implements GetAdvisories {
+            public function __construct(
+                private ?Advisory $advisory = null
+            ) {
+            }
+
+            public function setAdvisory(?Advisory $advisory): void
+            {
+                $this->advisory = $advisory;
+            }
+
+            /**
+             * @return Generator<Advisory>
+             */
+            public function __invoke(): Generator
+            {
+                yield Advisory::fromArrayData([
+                    'branches' => [
+                        '1.x' => [
+                            'time' => '2017-05-15 09:09:00',
+                            'versions' => ['<1.2'],
+                        ],
+                    ],
+                    'reference' => 'composer://3f/pygmentize',
+                ]);
+
+                yield Advisory::fromArrayData([
+                    'branches' => [
+                        '3.x' => [
+                            'time' => '2012-05-15 09:09:00',
+                            'versions' => ['<2|>4'],
+                        ],
+                    ],
+                    'reference' => 'composer://other/package-name',
+                ]);
+
+                if ($this->advisory === null) {
+                    return;
+                }
+
+                yield $this->advisory;
+            }
+        };
+    }
+}
